23475692b2bb717d9f11c0db9a517cc8f036c750
Edgar Adrián <adick.teddy@gmail.com>
2020-05-02 16:57:57 -0500

    Auto Layout constraint for Insert button

---
+++ iTahDoodle/iTahDoodle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTahDoodle
+//
+//  Created by Edgar Ruiz on 02/05/20.
+//  Copyright © 2020 Edgar Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
