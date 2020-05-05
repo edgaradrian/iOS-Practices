0eb55160da9acd3abde025bcdfe5c728bc6ad2ff
Edgar Adrián <adick.teddy@gmail.com>
2020-05-05 16:32:46 -0500

    Creating the Contact class

---
+++ Contacts/Contacts/Contact.swift
@@ -0,0 +1,20 @@
+//
+//  Contact.swift
+//  Contacts
+//
+//  Created by Edgar Ruiz on 05/05/20.
+//  Copyright © 2020 Edgar Ruiz. All rights reserved.
+//
+
+import Foundation
+
+class Contact: NSObject {
+    
+    let name: String
+    
+    init(name: String) {
+        self.name = name
+        super.init()
+    }//init
+    
+}//Contact
