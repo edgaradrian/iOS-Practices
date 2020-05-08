d545555315937c7a3795600836949f3de90afe8a
Edgar Adrián <adick.teddy@gmail.com>
2020-05-08 17:09:20 -0500

    Memory leak

--- RandomItemsObjectiveC/RandomItemsObjectiveC/main.m
+++ RandomItemsObjectiveC/RandomItemsObjectiveC/main.m
@@ -14,10 +14,16 @@ int main(int argc, const char * argv[]) {
         
         NSMutableArray *items = [[NSMutableArray alloc] init];
         
-        for (int i = 0; i < 10; i++) {
-            BNRItem *item = [BNRItem randomItem];
-            [items addObject:item];
-        }
+        BNRItem *backpack = [[BNRItem alloc] initWithItemName:@"Backpack"];
+        [items addObject:backpack];
+        
+        BNRItem *calculator = [[BNRItem alloc] initWithItemName:@"Calculator"];
+        [items addObject:calculator];
+        
+        backpack.containedItem = calculator;
+        
+        backpack = nil;
+        calculator = nil;
         
         for (BNRItem *item in items){
             NSLog(@"%@", item);
