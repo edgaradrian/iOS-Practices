e003bf8b655f447683f90ddbc884911cd722974a
Edgar Adrián <adick.teddy@gmail.com>
2020-05-08 20:42:03 -0500

    Adding the property declarations

--- RandomItemsObjectiveC/RandomItemsObjectiveC/BNRItem.h
+++ RandomItemsObjectiveC/RandomItemsObjectiveC/BNRItem.h
@@ -11,15 +11,14 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface BNRItem : NSObject
-{
-    NSString *_itemName;
-    NSString *_serialNumber;
-    int _valueInDollars;
-    NSDate *_dateCreated;
-    
-    BNRItem *_containedItem;
-    __weak BNRItem *_container;
-}
+
+@property BNRItem *containedItem;
+@property BNRItem *container;
+
+@property NSString *itemName;
+@property NSString *serialNumber;
+@property int valueInDollars;
+@property NSDate *dateCreated;
 
 +(instancetype)randomItem;
 
@@ -33,22 +32,6 @@ NS_ASSUME_NONNULL_BEGIN
 -(instancetype)initWithItemName:(NSString *)name
                    serialNumber:(NSString *)sNumber;
 
--(void)setContainedItem:(BNRItem *)item;
--(BNRItem *)containedItem;
-
--(void)setContainer:(BNRItem *)item;
--(BNRItem *)container;
-
--(void)setItemName:(NSString *)str;
--(NSString *)itemName;
-
--(void)setSerialNumber:(NSString *)str;
--(NSString *)serialNumber;
-
--(void)setValueInDollars:(int)v;
--(int)valueInDollars;
-
--(NSDate *)dateCreated;
 
 @end
 
