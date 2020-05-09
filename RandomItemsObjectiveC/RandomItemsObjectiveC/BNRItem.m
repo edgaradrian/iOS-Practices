890f9eda9f3dccae73966326a83592b7167eac03
Edgar Adrián <adick.teddy@gmail.com>
2020-05-08 20:44:19 -0500

    Deleting the accessor implementations.

--- RandomItemsObjectiveC/RandomItemsObjectiveC/BNRItem.m
+++ RandomItemsObjectiveC/RandomItemsObjectiveC/BNRItem.m
@@ -81,53 +81,6 @@
     NSString *descriptionString = [[NSString alloc] initWithFormat:@"%@ (%@): Worth $%d, recorded on %@", self.itemName, self.serialNumber, self.valueInDollars, self.dateCreated];
     return descriptionString;
 }//description
--(void)setItemName:(NSString *)str
-{
-    _itemName = str;
-}//setItemName
--(NSString *)itemName
-{
-    return _itemName;
-}//itemName
--(void)setSerialNumber:(NSString *)str
-{
-    _serialNumber = str;
-}//setSerialNumber
--(NSString *)serialNumber
-{
-    return _serialNumber;
-}//serialNumber
--(void)setValueInDollars:(int)v
-{
-    _valueInDollars = v;
-}//setValueInDollars
--(int)valueInDollars
-{
-    return _valueInDollars;
-}//valueInDollars
--(NSDate *)dateCreated
-{
-    return _dateCreated;
-}//dateCreated
-
--(void)setContainedItem:(BNRItem *)item
-{
-    _containedItem = item;
-    
-    item.container = self;
-}//setContainedItem
--(BNRItem *)containedItem
-{
-    return _containedItem;
-}//containedItem
--(void)setContainer:(BNRItem *)item
-{
-    _container = item;
-}//setContainer
--(BNRItem *)container
-{
-    return _container;
-}//container
 
 
 @end
