5bd0fd7c495f2926d190f9b1d969149e3fa4cd91
Edgar Adrián <adick.teddy@gmail.com>
2020-05-08 21:03:38 -0500

    Custom accessors with properties

--- RandomItemsObjectiveC/RandomItemsObjectiveC/BNRItem.m
+++ RandomItemsObjectiveC/RandomItemsObjectiveC/BNRItem.m
@@ -82,5 +82,10 @@
     return descriptionString;
 }//description
 
+-(void)setContainedItem:(BNRItem *)containedItem
+{
+    _containedItem = containedItem;
+    self.containedItem.container = self;
+}//setContainedItem
 
 @end
