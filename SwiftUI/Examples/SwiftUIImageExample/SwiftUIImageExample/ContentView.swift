feffc705fbb26c46bd4155afa36d55169913d2e3
Edgar Adrian <adick.teddy@gmail.com>
2020-05-25 21:57:05 -0500

    changing its color to red

--- SwiftUI/Examples/SwiftUIImageExample/SwiftUIImageExample/ContentView.swift
+++ SwiftUI/Examples/SwiftUIImageExample/SwiftUIImageExample/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     var body: some View {
         Image(systemName: "cloud.heavyrain")
             .font(.system(size: 100))
+            .foregroundColor(.red)
     }
 }
 
