41762a81d8364df973784e97d9d6d99c6fd9c688
Edgar Adrian <adick.teddy@gmail.com>
2020-05-25 21:51:36 -0500

    Displaying a System Image

---
+++ SwiftUI/Examples/SwiftUIImageExample/SwiftUIImageExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIImageExample
+//
+//  Created by Edgar Adrián on 25/05/20.
+//  Copyright © 2020 Edgar Adrián. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image(systemName: "cloud.heavyrain")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
