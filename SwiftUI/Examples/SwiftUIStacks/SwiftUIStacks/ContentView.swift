71af5b454088efda1ef6d1586333c93abc20f781
Edgar Adrian <adick.teddy@gmail.com>
2020-05-28 15:03:43 -0500

    Putting the heading

---
+++ SwiftUI/Examples/SwiftUIStacks/SwiftUIStacks/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  SwiftUIStacks
+//
+//  Created by Edgar Adrián on 28/05/20.
+//  Copyright © 2020 Edgar Adrián. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Choose")
+                .font(.system(.largeTitle, design: .rounded))
+                .fontWeight(.black)
+            Text("Your Plan")
+                .font(.system(.largeTitle, design: .rounded))
+                .fontWeight(.black)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
