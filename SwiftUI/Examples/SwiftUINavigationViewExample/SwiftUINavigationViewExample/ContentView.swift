05359d574f840a4fe75e8963a91c3956d545b93c
Edgar Adrian <adick.teddy@gmail.com>
2020-05-29 17:32:15 -0500

    Adding a Second View

---
+++ SwiftUI/Examples/SwiftUINavigationViewExample/SwiftUINavigationViewExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUINavigationViewExample
+//
+//  Created by Edgar Adrián on 29/05/20.
+//  Copyright © 2020 Edgar Adrián. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
