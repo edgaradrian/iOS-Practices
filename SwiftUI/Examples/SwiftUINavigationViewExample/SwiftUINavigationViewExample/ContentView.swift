ea449cdb60902ad817e11b465451338aa712a79f
Edgar Adrian <adick.teddy@gmail.com>
2020-05-29 17:45:00 -0500

    Using NavigationView  and NavigationLink

--- SwiftUI/Examples/SwiftUINavigationViewExample/SwiftUINavigationViewExample/ContentView.swift
+++ SwiftUI/Examples/SwiftUINavigationViewExample/SwiftUINavigationViewExample/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            NavigationLink(destination: SecondView()) {
+                Text("Go to Second View")
+            }
+        }
     }
 }
 
--- SwiftUI/Examples/SwiftUINavigationViewExample/SwiftUINavigationViewExample/SecondView.swift
+++ SwiftUI/Examples/SwiftUINavigationViewExample/SwiftUINavigationViewExample/SecondView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct SecondView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("I am The Second View")
     }
 }
 
