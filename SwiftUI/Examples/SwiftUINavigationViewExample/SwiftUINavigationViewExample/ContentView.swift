e8896b2a99f4526221acdb13dc48c2aae6cf779a
Edgar Adrian <adick.teddy@gmail.com>
2020-05-29 17:49:10 -0500

    Using navigationBarTitle modifier

--- SwiftUI/Examples/SwiftUINavigationViewExample/SwiftUINavigationViewExample/ContentView.swift
+++ SwiftUI/Examples/SwiftUINavigationViewExample/SwiftUINavigationViewExample/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
         NavigationView {
             NavigationLink(destination: SecondView()) {
                 Text("Go to Second View")
-            }
+            }.navigationBarTitle("Main View")
         }
     }
 }
