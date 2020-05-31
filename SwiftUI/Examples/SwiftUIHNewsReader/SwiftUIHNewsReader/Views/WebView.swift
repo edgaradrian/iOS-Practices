8e6e6822020c5b31307ba114b2e1c8fd6ffe4d7f
Edgar Adrián <adick.teddy@gmail.com>
2020-05-31 16:20:06 -0500

    HNewsReader completed

--- SwiftUI/Examples/SwiftUIHNewsReader/SwiftUIHNewsReader/Views/WebView.swift
+++ SwiftUI/Examples/SwiftUIHNewsReader/SwiftUIHNewsReader/Views/WebView.swift
@@ -13,12 +13,17 @@ struct WebView: UIViewRepresentable {
     
     let urlString: String?
     
-    func makeUIView(context: Context) -> some UIView {
+    func makeUIView(context: Context) -> WKWebView {
         return WKWebView()
     }
     
-    func updateUIView(_ uiView: some UIView, context: Context) {
-        
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
     }
     
 }
