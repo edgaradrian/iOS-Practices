24ad4f5f3e8f4047cc142ed35c717ede8a716db5
Edgar Adrián <adick.teddy@gmail.com>
2020-05-31 14:27:04 -0500

    Creating NetworkManager

---
+++ SwiftUI/Examples/SwiftUIHNewsReader/SwiftUIHNewsReader/Models/NetworkManager.swift
@@ -0,0 +1,26 @@
+//
+//  NetworkManager.swift
+//  SwiftUIHNewsReader
+//
+//  Created by Edgar Ruiz on 31/05/20.
+//  Copyright © 2020 Edgar Ruiz. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    func fetchData() {
+        
+        if let url = URL(string: "http://hn.algolia.com/api/v1/search?tags=front_page") {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error == nil {
+                    let decoder = JSONDecoder()
+                }
+            }
+        }
+    
+    }
+    
+}//class NetworkManager
