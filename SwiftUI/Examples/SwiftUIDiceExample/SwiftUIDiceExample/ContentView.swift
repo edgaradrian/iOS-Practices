8b08526b3d5cbc6f416749710103ebfe9ddc6f92
Edgar Adrián <adick.teddy@gmail.com>
2020-05-30 19:41:47 -0500

    Adding Spacer component

--- SwiftUI/Examples/SwiftUIDiceExample/SwiftUIDiceExample/ContentView.swift
+++ SwiftUI/Examples/SwiftUIDiceExample/SwiftUIDiceExample/ContentView.swift
@@ -10,12 +10,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Image("background")
+            .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(number: 4)
+                    DiceView(number: 6)
+                }
+                .padding()
+                Spacer()
+                Button(action: {
+                    
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+            }
+            
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    
+    let number: Int
+    
+    var body: some View {
+        Image("dice\(number)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
     }
 }
