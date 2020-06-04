8e1343754b7d79855c589270539fd246b8f10c83
Edgar Adrián <adick.teddy@gmail.com>
2020-06-04 15:11:47 -0500

    Applying the scaleEffect modifier

--- SwiftUI/Examples/SwiftUIButtonExample/SwiftUIButtonExample/GradientBackgroundStyle.swift
+++ SwiftUI/Examples/SwiftUIButtonExample/SwiftUIButtonExample/GradientBackgroundStyle.swift
@@ -19,6 +19,7 @@ struct GradientBackgroundStyle: ButtonStyle {
             .background(LinearGradient(gradient: Gradient(colors: [Color("DarkGreen"), Color("LightGreen")]), startPoint: .top, endPoint: .bottom))
             .cornerRadius(40)
             .padding(.horizontal, 20)
+            .scaleEffect(configuration.isPressed ? 0.9 : 1.0)
     }
     
     
