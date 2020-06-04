b0e9233b40089e3767e9e13aa629e39d2bdd4ae3
Edgar Adrián <adick.teddy@gmail.com>
2020-06-03 22:12:23 -0500

    Adding a CardView

---
+++ SwiftUI/Examples/SwiftUIScrollView/SwiftUIScrollView/CardView.swift
@@ -0,0 +1,21 @@
+//
+//  CardView.swift
+//  SwiftUIScrollView
+//
+//  Created by Edgar Ruiz on 03/06/20.
+//  Copyright © 2020 Edgar Ruiz. All rights reserved.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
