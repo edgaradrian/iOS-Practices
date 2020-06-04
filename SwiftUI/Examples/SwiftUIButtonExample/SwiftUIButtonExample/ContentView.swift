df7aea64612e3aa3ba2d1c1b03d03f7abb0ac789
Edgar Adrián <adick.teddy@gmail.com>
2020-06-04 12:34:30 -0500

    Creating a simple button

---
+++ SwiftUI/Examples/SwiftUIButtonExample/SwiftUIButtonExample/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  SwiftUIButtonExample
+//
+//  Created by Edgar Ruiz on 04/06/20.
+//  Copyright © 2020 Edgar Ruiz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Button(action: {
+            print("Holaaaaaa... Enfermera tapped")
+        }) {
+            Text("Holaaaaa.... Enfermera")
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
