33115feb36741bead3aaf82f8a316a6759673530
Edgar Adrián <adick.teddy@gmail.com>
2020-06-07 22:16:21 -0500

    Drawing the progress view

--- SwiftUI/Examples/SwiftUIProgressIndicatorShape/SwiftUIProgressIndicatorShape/ContentView.swift
+++ SwiftUI/Examples/SwiftUIProgressIndicatorShape/SwiftUIProgressIndicatorShape/ContentView.swift
@@ -17,6 +17,23 @@ struct ContentView: View {
             Circle()
                 .stroke(Color(.systemGray6), lineWidth: 20)
                 .frame(width: 300, height: 300)
+            
+            Circle()
+                .trim(from: 0, to: 0.85)
+                .stroke(purpleGradient, lineWidth: 20)
+                .frame(width: 300, height: 300)
+                .overlay(
+                    VStack {
+                        Text("85")
+                            .font(.system(size: 80, weight: .bold, design: .rounded))
+                            .foregroundColor(Color(.systemGray))
+                        Text("Complete")
+                            .font(.system(.body, design: .rounded))
+                            .bold()
+                            .foregroundColor(Color(.systemGray))
+                    }
+            )
+            
         }
     }
 }
