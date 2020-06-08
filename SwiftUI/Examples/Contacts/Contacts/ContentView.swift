c8dd4a4e22a6a5a482d6d10d1f880667283b906a
Edgar Adrián <adick.teddy@gmail.com>
2020-06-08 01:13:55 -0500

    Creating Core Data project

---
+++ SwiftUI/Examples/Contacts/Contacts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Contacts
+//
+//  Created by Edgar Ruiz on 08/06/20.
+//  Copyright © 2020 Edgar Ruiz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/Examples/Contacts/Contacts/Models/Contacts.swift
@@ -0,0 +1,9 @@
+//
+//  Contacts.swift
+//  Contacts
+//
+//  Created by Edgar Ruiz on 08/06/20.
+//  Copyright © 2020 Edgar Ruiz. All rights reserved.
+//
+
+import Foundation
