ccdf58e9837a19467b983cc941f2e3446ea6e7a8
Edgar Adrián <adick.teddy@gmail.com>
2020-06-07 21:22:09 -0500

    Creating a button with the Dome shape

--- SwiftUI/Examples/SwiftUIShapeExample/SwiftUIShapeExample/ContentView.swift
+++ SwiftUI/Examples/SwiftUIShapeExample/SwiftUIShapeExample/ContentView.swift
@@ -14,33 +14,52 @@ struct ContentView: View {
     
     var body: some View {
         
-        ZStack {
-            
-            Path() { path in
-                path.move(to: CGPoint(x: 200, y: 200))
-                path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 0.0), endAngle: Angle(degrees: 190), clockwise: true)
+        VStack {
+            ZStack {
+                
+                
+                Path() { path in
+                    path.move(to: CGPoint(x: 200, y: 200))
+                    path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 0.0), endAngle: Angle(degrees: 190), clockwise: true)
+                }
+                .fill(Color(.systemYellow))
+                
+                Path() { path in
+                    path.move(to: CGPoint(x: 200, y: 200))
+                    path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 190), endAngle: Angle(degrees: 110), clockwise: true)
+                }
+                .fill(Color(.systemTeal))
+                
+                Path() { path in
+                    path.move(to: CGPoint(x: 200, y: 200))
+                    path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 110), endAngle: Angle(degrees: 90), clockwise: true)
+                }
+                .fill(Color(.systemBlue))
+                
+                
+                Path() { path in
+                    path.move(to: CGPoint(x: 200, y: 200))
+                    path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 360), clockwise: true)
+                }
+                .fill(Color(.systemPink))
+                .offset(x: 20, y: 20)
+                
             }
-            .fill(Color(.systemYellow))
+            .layoutPriority(100)
             
-            Path() { path in
-                path.move(to: CGPoint(x: 200, y: 200))
-                path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 190), endAngle: Angle(degrees: 110), clockwise: true)
+            Button(action: {
+                print("action")
+            }) {
+                Text("Test")
+                    .font(.system(.title, design: .rounded))
+                .bold()
+                    .foregroundColor(.white)
+                    .frame(width: 250, height: 50)
+                    .background(Dome().fill(Color(.systemTeal)))
+                
             }
-            .fill(Color(.systemTeal))
             
-            Path() { path in
-                path.move(to: CGPoint(x: 200, y: 200))
-                path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 110), endAngle: Angle(degrees: 90), clockwise: true)
-            }
-            .fill(Color(.systemBlue))
-            
-            Path() { path in
-                path.move(to: CGPoint(x: 200, y: 200))
-                path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 360), clockwise: true)
-            }
-            .fill(Color(.systemPink))
-            .offset(x: 20, y: 20)
-            
+            Spacer()
         }
         
     }
