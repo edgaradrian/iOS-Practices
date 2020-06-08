48ff2d614874275403c639505b2d7ae89abe47ad
Edgar Adrián <adick.teddy@gmail.com>
2020-06-07 20:48:01 -0500

    A pie chart with a highlighted segment

--- SwiftUI/Examples/SwiftUIShapeExample/SwiftUIShapeExample/ContentView.swift
+++ SwiftUI/Examples/SwiftUIShapeExample/SwiftUIShapeExample/ContentView.swift
@@ -14,11 +14,34 @@ struct ContentView: View {
     
     var body: some View {
         
-        Path() { path in
-            path.move(to: origin)
-            path.addArc(center: origin, radius: 125, startAngle: Angle(degrees: 0.0), endAngle: Angle(degrees: 90), clockwise: true)
+        ZStack {
+            
+            Path() { path in
+                path.move(to: CGPoint(x: 200, y: 200))
+                path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 0.0), endAngle: Angle(degrees: 190), clockwise: true)
+            }
+            .fill(Color(.systemYellow))
+            
+            Path() { path in
+                path.move(to: CGPoint(x: 200, y: 200))
+                path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 190), endAngle: Angle(degrees: 110), clockwise: true)
+            }
+            .fill(Color(.systemTeal))
+            
+            Path() { path in
+                path.move(to: CGPoint(x: 200, y: 200))
+                path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 110), endAngle: Angle(degrees: 90), clockwise: true)
+            }
+            .fill(Color(.systemBlue))
+            
+            Path() { path in
+                path.move(to: CGPoint(x: 200, y: 200))
+                path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 360), clockwise: true)
+            }
+            .fill(Color(.systemPink))
+            .offset(x: 20, y: 20)
+            
         }
-        .fill(Color.blue)
         
     }
 }
