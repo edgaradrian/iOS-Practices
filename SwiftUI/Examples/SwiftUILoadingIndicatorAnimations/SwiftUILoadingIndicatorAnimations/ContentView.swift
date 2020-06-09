07a6667dd1effef79804489e5f65e965f5508272
Edgar Adrián <adick.teddy@gmail.com>
2020-06-09 18:15:19 -0500

    a sample loading indicator

--- SwiftUI/Examples/SwiftUILoadingIndicatorAnimations/SwiftUILoadingIndicatorAnimations/ContentView.swift
+++ SwiftUI/Examples/SwiftUILoadingIndicatorAnimations/SwiftUILoadingIndicatorAnimations/ContentView.swift
@@ -13,14 +13,21 @@ struct ContentView: View {
     @State private var isLoading = false
     
     var body: some View {
-        Circle()
-            .trim(from: 0.0, to: 0.7)
-            .stroke(Color.blue, lineWidth: 8)
-            .frame(width: 100, height: 100)
-            .rotationEffect(Angle(degrees: isLoading ? 360 : 0))
-            .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
-            .onAppear() {
-                self.isLoading = true
+        ZStack {
+            
+            Circle()
+                .stroke(Color(.systemGray6), lineWidth: 20)
+                .frame(width: 100, height: 100)
+            
+            Circle()
+                .trim(from: 0.0, to: 0.7)
+                .stroke(Color.blue, lineWidth: 8)
+                .frame(width: 100, height: 100)
+                .rotationEffect(Angle(degrees: isLoading ? 360 : 0))
+                .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+                .onAppear() {
+                    self.isLoading = true
+            }
         }
         
     }
