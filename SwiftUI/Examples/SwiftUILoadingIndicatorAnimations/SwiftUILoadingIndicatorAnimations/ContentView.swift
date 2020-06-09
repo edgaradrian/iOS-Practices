70006a543cc2e43b0679384d59f4fac1de116d6d
Edgar Adrián <adick.teddy@gmail.com>
2020-06-09 15:56:59 -0500

    Repeating the same animation again and again

--- SwiftUI/Examples/SwiftUILoadingIndicatorAnimations/SwiftUILoadingIndicatorAnimations/ContentView.swift
+++ SwiftUI/Examples/SwiftUILoadingIndicatorAnimations/SwiftUILoadingIndicatorAnimations/ContentView.swift
@@ -9,11 +9,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isLoading = false
+    
     var body: some View {
         Circle()
             .trim(from: 0.0, to: 0.7)
             .stroke(Color.blue, lineWidth: 8)
             .frame(width: 100, height: 100)
+            .rotationEffect(Angle(degrees: isLoading ? 360 : 0))
+            .animation(Animation.default.repeatForever(autoreverses: false))
+            .onAppear() {
+                self.isLoading = true
+        }
+        
     }
 }
 
