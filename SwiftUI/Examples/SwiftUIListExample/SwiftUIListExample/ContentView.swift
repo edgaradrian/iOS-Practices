e681c5d4b1c1d0d36d29b3155de23f2abcd7667e
Edgar Adrián <adick.teddy@gmail.com>
2020-06-11 22:58:05 -0500

    Declaring two arrays in Content View

--- SwiftUI/Examples/SwiftUIListExample/SwiftUIListExample/ContentView.swift
+++ SwiftUI/Examples/SwiftUIListExample/SwiftUIListExample/ContentView.swift
@@ -9,6 +9,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petit e Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli" , "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donos tia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petite oyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate" , "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "five leaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpub kitchen"]
+    
     var body: some View {
         List {
             ForEach(1...5, id: \.self) {
