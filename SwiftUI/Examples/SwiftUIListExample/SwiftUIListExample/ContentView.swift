bedbeb5bd37ddebaba4325532e5c650ae8d51678
Edgar Adrián <adick.teddy@gmail.com>
2020-06-11 23:13:19 -0500

    A list view with custom row layout

--- SwiftUI/Examples/SwiftUIListExample/SwiftUIListExample/ContentView.swift
+++ SwiftUI/Examples/SwiftUIListExample/SwiftUIListExample/ContentView.swift
@@ -12,12 +12,18 @@ struct ContentView: View {
     
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petit e Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli" , "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donos tia", "Royal Oak", "CASK Pub and Kitchen"]
     
-    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petite oyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate" , "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "five leaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpub kitchen"]
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate" , "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpubkitchen"]
     
     var body: some View {
         List {
-            ForEach(1...5, id: \.self) {
-                Text("\($0)")
+            ForEach(restaurantNames.indices, id: \.self) { index in
+                HStack {
+                    Image(self.restaurantImages[index])
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .cornerRadius(5)
+                    Text(self.restaurantNames[index])
+                }
             }
         }
     }
