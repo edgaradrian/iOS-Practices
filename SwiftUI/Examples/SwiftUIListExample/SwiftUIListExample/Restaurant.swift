a02b0cc089112920d96366d92540045943e041c0
Edgar Adrián <adick.teddy@gmail.com>
2020-06-12 14:00:58 -0500

    Creatin a Restaurant struct

---
+++ SwiftUI/Examples/SwiftUIListExample/SwiftUIListExample/Restaurant.swift
@@ -0,0 +1,14 @@
+//
+//  Restaurant.swift
+//  SwiftUIListExample
+//
+//  Created by Edgar Ruiz on 12/06/20.
+//  Copyright © 2020 Edgar Ruiz. All rights reserved.
+//
+
+import SwiftUI
+
+struct Restaurant {
+    var name: String
+    var image: String
+}//Restaurant
