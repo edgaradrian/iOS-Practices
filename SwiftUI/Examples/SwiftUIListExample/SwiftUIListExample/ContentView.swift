d04b7bbb411dd85b788fff13cfd951dcb78d7bfb
Edgar Adrián <adick.teddy@gmail.com>
2020-06-12 14:19:33 -0500

    Using the Restaurant struct

--- SwiftUI/Examples/SwiftUIListExample/SwiftUIListExample/ContentView.swift
+++ SwiftUI/Examples/SwiftUIListExample/SwiftUIListExample/ContentView.swift
@@ -10,19 +10,15 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petit e Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli" , "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donos tia", "Royal Oak", "CASK Pub and Kitchen"]
-    
-    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate" , "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpubkitchen"]
-    
     var body: some View {
         List {
-            ForEach(restaurantNames.indices, id: \.self) { index in
+            ForEach(restaurants, id: \.name) { restaurant in
                 HStack {
-                    Image(self.restaurantImages[index])
+                    Image(restaurant.image)
                         .resizable()
                         .frame(width: 40, height: 40)
                         .cornerRadius(5)
-                    Text(self.restaurantNames[index])
+                    Text(restaurant.name)
                 }
             }
         }
