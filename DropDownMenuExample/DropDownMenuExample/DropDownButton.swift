888ba28613ffbb5c5dea836aa51816a6ba5563ba
Edgar Adrián <adick.teddy@gmail.com>
2020-07-28 16:49:55 -0500

    Adding DropMenuButton class

---
+++ DropDownMenuExample/DropDownMenuExample/DropDownButton.swift
@@ -0,0 +1,21 @@
+//
+//  DropDownButton.swift
+//  DropDownMenuExample
+//
+//  Created by Edgar Ruiz on 28/07/20.
+//  Copyright © 2020 Edgar Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class DropDownButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }//init
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }//init? coder
+    
+}//DropDownButton
---
+++ DropDownMenuExample/DropDownMenuExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DropDownMenuExample
+//
+//  Created by Edgar Ruiz on 28/07/20.
+//  Copyright © 2020 Edgar Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
