1480ed04dc7bfb2d2055f0f63454b5de503aa606
Edgar Adrián <adick.teddy@gmail.com>
2020-07-28 19:44:20 -0500

    Adding the dropDownView to DropDownButton

--- DropDownMenuExample/DropDownMenuExample/DropDownButton.swift
+++ DropDownMenuExample/DropDownMenuExample/DropDownButton.swift
@@ -10,14 +10,39 @@ import UIKit
 
 class DropDownButton: UIButton {
     
+    var dropDownView = DropDownView()
+    var heightConstraint = NSLayoutConstraint()
+    var isOpen = false
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        self.backgroundColor = UIColor.yellow
+        self.backgroundColor = UIColor.gray
+        
+        dropDownView = DropDownView.init(frame: CGRect.init(x: 0, y: 0, width: 0, height: 0))
+        dropDownView.translatesAutoresizingMaskIntoConstraints = false
+ 
     }//init
+    
+    override func didMoveToSuperview() {
+        self.superview?.addSubview(dropDownView)
+        self.superview?.bringSubviewToFront(dropDownView)
+        dropDownView.topAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        dropDownView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        dropDownView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        heightConstraint = dropDownView.heightAnchor.constraint(equalToConstant: 0)
+    }//didMoveToSuperview
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }//init? coder
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if isOpen {
+            
+        } else {
+            
+        }
+    }
     
 }//DropDownButton
