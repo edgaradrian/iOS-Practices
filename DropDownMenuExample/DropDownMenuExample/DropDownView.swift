6d1807ffd97c85f4c3cdbf94d3f7c80ffd39da28
Edgar Adrián <adick.teddy@gmail.com>
2020-07-28 19:18:01 -0500

    Set up DropDownView

---
+++ DropDownMenuExample/DropDownMenuExample/DropDownView.swift
@@ -0,0 +1,58 @@
+//
+//  DropDownView.swift
+//  DropDownMenuExample
+//
+//  Created by Edgar Ruiz on 28/07/20.
+//  Copyright © 2020 Edgar Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class DropDownView: UIView {
+    
+    var dropDownOptions = [String]()
+    var tableView = UITableView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(tableView)
+        
+        tableView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        
+    }//init
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }//init?
+      
+}//class DropDownView
+
+extension DropDownView: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }//numberOfSections
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dropDownOptions.count
+    }//numberOfRowsInSection
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = dropDownOptions[indexPath.row]
+        return cell
+    }//cellForRowAt
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("selected: \(dropDownOptions[indexPath.row])")
+    }
+    
+}//extension DropDownView
