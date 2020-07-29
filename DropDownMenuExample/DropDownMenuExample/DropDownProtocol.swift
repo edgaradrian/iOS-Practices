535b95484ed62e52665efd5adc24b7db838188d4
Edgar Adrián <adick.teddy@gmail.com>
2020-07-28 20:21:06 -0500

    Creating DropDownProtocol

---
+++ DropDownMenuExample/DropDownMenuExample/DropDownProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  DropDownProtocol.swift
+//  DropDownMenuExample
+//
+//  Created by Edgar Ruiz on 28/07/20.
+//  Copyright © 2020 Edgar Ruiz. All rights reserved.
+//
+
+import Foundation
+
+protocol DropDownProtocol {
+    func dropDownButtonPressed(value: String)
+}//DropDownProtocol
