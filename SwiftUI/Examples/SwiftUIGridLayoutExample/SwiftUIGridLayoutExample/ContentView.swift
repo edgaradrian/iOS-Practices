1b98cd58c58ce2a73c59e219c20980ebf0bdb5b6
Edgar Adrian <adick.teddy@gmail.com>
2020-08-20 21:13:13 -0500

    Creating an array of SF symbols that I am going to present in the app

---
+++ SwiftUI/Examples/SwiftUIGridLayoutExample/SwiftUIGridLayoutExample/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIGridLayoutExample
+//
+//  Created by Edgar Adrián on 20/08/20.
+//  Copyright © 2020 Edgar Adrián. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    private var symbols = ["keyboard", "hifispeaker.fill", "printer.fill", "tv.fill", "desktopcomputer", "headphones", "tv.music.note", "mic", "plus.bubble", "video"]
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
