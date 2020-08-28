180d5ab72c1dfe4593dcc59c5a5149bb2670821c
Edgar Adrian <adick.teddy@gmail.com>
2020-08-27 20:22:46 -0500

    Creating a list view with LazyVGrid

--- SwiftUI/Examples/SwiftUIPhotoGrid/SwiftUIPhotoGrid/ContentView.swift
+++ SwiftUI/Examples/SwiftUIPhotoGrid/SwiftUIPhotoGrid/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var gridLayout: [GridItem] = [ GridItem() ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: gridLayout, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 10) {
+                    ForEach(samplePhotos.indices) { index in
+                        
+                        Image(samplePhotos[index].name)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .frame(height: 200)
+                            .cornerRadius(10)
+                            .shadow(color: Color.primary.opacity(0.3),radius: 1)
+                        
+                    }
+                }
+                .padding(.all, 10)
+            }
+            .navigationTitle("Coffee Feed")
+        }
+        
     }
 }
 
