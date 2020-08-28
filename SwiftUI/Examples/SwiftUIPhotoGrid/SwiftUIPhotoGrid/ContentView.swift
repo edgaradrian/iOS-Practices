2ffdb190957bb0b9b39d5cca515d44232bc8716e
Edgar Adrian <adick.teddy@gmail.com>
2020-08-27 19:53:05 -0500

    Adding images to the asset catalog

---
+++ SwiftUI/Examples/SwiftUIPhotoGrid/SwiftUIPhotoGrid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPhotoGrid
+//
+//  Created by Edgar Adrián on 27/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/Examples/SwiftUIPhotoGrid/SwiftUIPhotoGrid/SwiftUIPhotoGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPhotoGridApp.swift
+//  SwiftUIPhotoGrid
+//
+//  Created by Edgar Adrián on 27/08/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPhotoGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
