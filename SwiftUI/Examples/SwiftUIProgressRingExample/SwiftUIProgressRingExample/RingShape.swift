079b4fd317cbe008aae7c6f04112ae36f2cb6230
Edgar Adrian <adick.teddy@gmail.com>
2020-08-31 18:05:25 -0500

    Changing the startAngle parament to -90 degree.

--- SwiftUI/Examples/SwiftUIProgressRingExample/SwiftUIProgressRingExample/RingShape.swift
+++ SwiftUI/Examples/SwiftUIProgressRingExample/SwiftUIProgressRingExample/RingShape.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct RingShape: Shape {
     var progress: Double = 0.0
     var thickness: CGFloat = 30.0
+    var startAngle: Double = -90.0
     
     func path(in rect: CGRect) -> Path {
     
@@ -17,7 +18,7 @@ struct RingShape: Shape {
         
         path.addArc(center: CGPoint(x: rect.width / 2.0, y: rect.height / 2.0),
                     radius: min(rect.width, rect.height) / 2.0,
-                    startAngle: .degrees(0),
+                    startAngle: .degrees(startAngle),
                     endAngle: .degrees(360 * progress),
                     clockwise: false)
         
