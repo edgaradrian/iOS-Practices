f2542dd13627d25818e21701f169403651ef97fe
Edgar Adrian <adick.teddy@gmail.com>
2020-09-02 18:24:39 -0500

    Creating a quick demo

--- SwiftUI/Examples/SwiftUIProgressRingExample/SwiftUIProgressRingExample/ContentView.swift
+++ SwiftUI/Examples/SwiftUIProgressRingExample/SwiftUIProgressRingExample/ContentView.swift
@@ -12,8 +12,36 @@ struct ContentView: View {
     @State var progress = 0.0
     
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            ProgressRingView(progress: $progress)
+            
+            HStack {
+                Group {
+                    Text("0%")
+                        .font(.system(.headline, design: .rounded))
+                        .onTapGesture {
+                            self.progress = 0.0
+                        }
+                    
+                    Text("50%")
+                        .font(.system(.headline, design: .rounded))
+                        .onTapGesture {
+                            self.progress = 0.5
+                        }
+                    
+                    Text("100%")
+                        .font(.system(.headline, design: .rounded))
+                        .onTapGesture {
+                            self.progress = 1.0
+                        }
+                }
+                .padding()
+                .background(Color(.systemGray6))
+                .clipShape(RoundedRectangle(cornerRadius: 15.0, style: .continuous))
+                .padding()
+            }
             .padding()
+        }
     }
 }
 
