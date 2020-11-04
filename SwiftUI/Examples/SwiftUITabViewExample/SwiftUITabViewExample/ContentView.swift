e64533bf72febe03536ec84730227f93c75b8058
Edgar Adrián <adick.teddy@gmail.com>
2020-11-03 18:07:47 -0600

    Realm with SwuiftUI Example

---
+++ SwiftUI/Examples/SwiftUITabViewExample/SwiftUITabViewExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITabViewExample
+//
+//  Created by Edgar Ruiz on 03/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("My First TabView")
+            .font(.system(size: 30, weight: .bold, design: .rounded))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/Examples/SwiftUITabViewExample/SwiftUITabViewExample/SwiftUITabViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITabViewExampleApp.swift
+//  SwiftUITabViewExample
+//
+//  Created by Edgar Ruiz on 03/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITabViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
