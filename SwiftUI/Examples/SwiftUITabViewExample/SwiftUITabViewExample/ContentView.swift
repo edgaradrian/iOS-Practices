06420158b0b18e5b6c293efcffb077d78575c582
Edgar Adrián <adick.teddy@gmail.com>
2020-11-04 23:24:24 -0600

    Switching between tabs

--- SwiftUI/Examples/SwiftUITabViewExample/SwiftUITabViewExample/ContentView.swift
+++ SwiftUI/Examples/SwiftUITabViewExample/SwiftUITabViewExample/ContentView.swift
@@ -8,26 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selection = 0
+    
     var body: some View {
-        TabView {
-            Text("My First TabView")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "house.fill")
-                    Text("Home")
-                }
+        
+        ZStack(alignment: .topTrailing) {
             
-            Text("Profile Tab")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "person.crop.circle")
-                    Text("Profile")
-                }
-        }
-        .onAppear() {
-            UITabBar.appearance().barTintColor = .white
-        }
-        .accentColor(.pink)
+            TabView(selection: $selection) {
+                Text("My First TabView")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                        Text("Home")
+                    }
+                    .tag(0)
+                
+                Text("Profile Tab")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "person.crop.circle")
+                        Text("Profile")
+                    }
+                    .tag(1)
+            }
+            .onAppear() {
+                UITabBar.appearance().barTintColor = .white
+            }
+            .accentColor(.pink)
+            
+            Button(action: {
+                selection = (selection + 1) % 2
+            }) {
+                Text("Next")
+                    .font(.system(.headline, design: .rounded))
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.pink)
+                    .cornerRadius(10)
+                    .padding()
+            }
+            
+            
+        }//ZStack
+        
         
     }
 }
