610c64c1037dc8b78c0a8bf7c657436a97e8421a
Edgar Adrian <adick.teddy@gmail.com>
2020-12-02 23:41:16 -0600

    Creating the View

---
+++ SwiftUI/Examples/MVVM/HelloMVVM/HelloMVVM/HelloMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloMVVMApp.swift
+//  HelloMVVM
+//
+//  Created by Edgar Adrián on 02/12/20.
+//
+
+import SwiftUI
+
+@main
+struct HelloMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
