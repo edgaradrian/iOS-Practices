debb468363e88491167d4ebb9da173bc4e77d225
Edgar Adrian <adick.teddy@gmail.com>
2020-12-07 15:17:10 -0600

    Creating FailedView

---
+++ SwiftUI/Examples/MVVM/ListMoviesApp/ListMoviesApp/Views/FailedView.swift
@@ -0,0 +1,20 @@
+//
+//  FailedView.swift
+//  ListMoviesApp
+//
+//  Created by Edgar Adrián on 07/12/20.
+//
+
+import SwiftUI
+
+struct FailedView: View {
+    var body: some View {
+        Image("oops")
+    }
+}
+
+struct FailedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FailedView()
+    }
+}
