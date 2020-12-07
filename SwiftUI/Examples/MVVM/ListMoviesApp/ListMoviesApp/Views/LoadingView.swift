5b6f8693440ff0c4bb1cdeb388282a9431ec5b8b
Edgar Adrian <adick.teddy@gmail.com>
2020-12-07 15:49:14 -0600

    Creating LoadingView

---
+++ SwiftUI/Examples/MVVM/ListMoviesApp/ListMoviesApp/Views/LoadingView.swift
@@ -0,0 +1,21 @@
+//
+//  LoadingView.swift
+//  ListMoviesApp
+//
+//  Created by Edgar Adrián on 07/12/20.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text("Loading...")
+            .font(.largeTitle)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
