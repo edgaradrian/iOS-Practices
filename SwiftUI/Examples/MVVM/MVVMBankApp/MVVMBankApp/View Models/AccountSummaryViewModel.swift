0d7a5f06e23b6e5385c84c7b1a729556a6995699
Edgar Adrian <adick.teddy@gmail.com>
2020-12-15 14:58:34 -0600

    Creating Account and AccountSummary Model Views

--- SwiftUI/Examples/MVVM/MVVMBankApp/MVVMBankApp/View Models/AccountSummaryViewModel.swift
+++ SwiftUI/Examples/MVVM/MVVMBankApp/MVVMBankApp/View Models/AccountSummaryViewModel.swift
@@ -9,16 +9,25 @@ import Foundation
 
 class AccountSummaryViewModel: ObservableObject {
     
+    private var _accounts = [Account]()
+    
     var accounts: [AccountViewModel] = [AccountViewModel]()
+    
+    var total: Double {
+        _accounts.map { $0.balance }.reduce(0, +)
+    }
     
     func getAllAccounts() {
         
         AccountService.shared.getAllAccounts { (result) in
             switch result {
             case .success(let accounts):
-                print("Success")
+                if let accounts = accounts {
+                    self._accounts = accounts
+                    self.accounts = accounts.map(AccountViewModel.init)
+                }
             case .failure(let error):
-                print("Failure")
+                print(error.localizedDescription)
             }
         }
     }
