9a295f59fde2f11bcbd6064b19b412c856b6a9c0
Edgar Adrian <adick.teddy@gmail.com>
2020-12-15 21:09:14 -0600

    Preparing for Processing

--- MachineLearning/Chapter2/Chapter2/ViewController.swift
+++ MachineLearning/Chapter2/Chapter2/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import AVKit
+import Vision
+import CoreML
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var predictionLabel: UILabel!
+    @IBOutlet weak var confidenceLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.setupSession()
     }
 
+}//ViewController
 
-}
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    func setupSession() {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: device) else { return }
+        
+        let session = AVCaptureSession()
+        session.sessionPreset = .hd1920x1080
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.frame = view.frame
+        previewLayer.videoGravity = .resizeAspectFill
+        imageView.layer.addSublayer(previewLayer)
+        
+        let output = AVCaptureVideoDataOutput()
+        output.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        
+        session.addOutput(output)
+        session.addInput(input)
+        session.startRunning()
+        
+    }//setupSession
+    
+}//extension AVCaptureVideoDataOutputSampleBufferDelegate
 
