b2eb3eef761ae13f036476023309b26633ec79e2
Edgar Adrian <adick.teddy@gmail.com>
2020-12-30 14:31:57 -0600

    Implementing TransferFundsViewModel

---
+++ SwiftUI/Examples/MVVM/MVVMBankApp/MVVMBankApp/View Models/TransferFundsViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  TransferFundsViewModel.swift
+//  MVVMBankApp
+//
+//  Created by Edgar Adrián on 30/12/20.
+//
+
+import Foundation
+
+class TransferFundsViewModel: ObservableObject {
+    
+    @Published var accounts = [AccountViewModel]()
+    var fromAccount: AccountViewModel?
+    var toAccount: AccountViewModel?
+    
+    func populateAccounts() {
+        AccountService.shared.getAllAccounts { (result) in
+            switch result {
+            case .success(let accounts):
+                if let accounts = accounts {
+                    DispatchQueue.main.async {
+                        self.accounts = accounts.map(AccountViewModel.init)
+                    }
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+                
+            }
+        }
+    }
+    
+}
