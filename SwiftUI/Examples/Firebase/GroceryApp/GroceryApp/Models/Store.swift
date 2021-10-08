e9357333810c7b5f9be582393c64d4399b79661e
Edgar Adrián <adick.teddy@gmail.com>
2021-10-07 20:52:08 -0500

    Creating Store model

---
+++ SwiftUI/Examples/Firebase/GroceryApp/GroceryApp/Models/Store.swift
@@ -0,0 +1,16 @@
+//
+//  Store.swift
+//  GroceryApp
+//
+//  Created by Edgar Ruiz on 07/10/21.
+//
+
+import Foundation
+
+struct Store: Codable {
+    
+    var id: String?
+    let name: String
+    let address: String
+    
+}//Store
