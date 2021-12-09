ac1bf79c863440d8e81687463b7a31fb4fef61d8
Edgar Adrián <adick.teddy@gmail.com>
2021-12-09 10:50:41 -0600

    implementing Store Model and Firestore save

---
+++ SwiftUI/Examples/Firebase/GroceryApp/GroceryApp/Managers/FirestoreManager.swift
@@ -0,0 +1,38 @@
+//
+//  FirestoreManager.swift
+//  GroceryApp
+//
+//  Created by Edgar Ruiz on 07/10/21.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+class FirestoreManager {
+    
+    private var db: Firestore
+    
+    init() {
+        db = Firestore.firestore()
+    }//init
+    
+    func save(store: Store, completion: @escaping (Result<Store?, Error>) -> Void) {
+        
+        do {
+            let ref = try db.collection("stores").addDocument(from: store)
+            ref.getDocument { (snapshot, error) in
+                guard let snapshot = snapshot, error == nil else {
+                    completion(.failure(error!))
+                    return
+                }
+                let store = try? snapshot.data(as: Store.self)
+                completion(.success(store))
+            }
+        } catch let error {
+            completion(.failure(error))
+        }
+        
+    }//save
+    
+}//FirestoreManager
