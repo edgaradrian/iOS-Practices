e7dd24b49466d9865242828d106da669e59f74b0
Edgar Adrián <adick.teddy@gmail.com>
2022-03-20 03:37:29 -0600

    TabView - Paging Style Index Display Mode

---
+++ SwiftUI/Examples/MyFirstBasicTemplate/MyFirstBasicTemplate/TabView/TabView_PageTabViewStyle_IndexDisplayMode.swift
@@ -0,0 +1,42 @@
+//
+//  TabView_PageTabViewStyle_IndexDisplayMode.swift
+//  MyFirstBasicTemplate
+//
+//  Created by Edgar Ruiz on 20/03/22.
+//
+
+import SwiftUI
+
+struct TabView_PageTabViewStyle_IndexDisplayMode: View {
+    var body: some View {
+        TabView {
+            VStack(spacing: 20) {
+                HeaderView(titulo: "TabView", subtitulo: "Index Display Mode", description: "También se pueden quitar los puntos de navegación con el parámetro indexDisplayMode y colocando .never")
+                
+                Text("Página 1")
+                
+                Spacer()
+                
+                DescView(description: "También hay existe el autómatico para mostrar los puntos de navegación.", backColor: .blue)
+                
+            }
+            
+            VStack {
+                Text("Página 2")
+            }
+            
+            VStack {
+                Text("Página 3")
+            }
+            
+        }
+        .font(.title)
+        .tabViewStyle(.page(indexDisplayMode: .never))
+    }
+}
+
+struct TabView_PageTabViewStyle_IndexDisplayMode_Previews: PreviewProvider {
+    static var previews: some View {
+        TabView_PageTabViewStyle_IndexDisplayMode()
+    }
+}
