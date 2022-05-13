e6bf118ed13a00e39cea1607e1012b9736ab9fe1
Edgar Adrián <adick.teddy@gmail.com>
2022-05-13 07:27:54 -0500

    Creating a Circle shape

--- SwiftUI/Animations/MyFirstSwiftUIAnimation/MyFirstSwiftUIAnimation/MyFirstSwiftUIAnimation/ContentView.swift
+++ SwiftUI/Animations/MyFirstSwiftUIAnimation/MyFirstSwiftUIAnimation/MyFirstSwiftUIAnimation/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Circle()
+                .scaleEffect(0.5)
+                .foregroundColor(.cyan)
+        }
+        .onAppear {
+            
+        }
     }
 }
 
