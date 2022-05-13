129b8fb505ad67fd5ca81a93720994f1edd7969e
Edgar Adrián <adick.teddy@gmail.com>
2022-05-13 07:49:58 -0500

    Changing color by offset counting

--- SwiftUI/Animations/MyFirstSwiftUIAnimation/MyFirstSwiftUIAnimation/MyFirstSwiftUIAnimation/ContentView.swift
+++ SwiftUI/Animations/MyFirstSwiftUIAnimation/MyFirstSwiftUIAnimation/MyFirstSwiftUIAnimation/ContentView.swift
@@ -9,24 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var colour = Color.cyan
+    @State private var currentOffset = 0
     
     var body: some View {
         ZStack {
             Circle()
                 .scaleEffect(0.5)
-                .foregroundColor(colour)
-                .animation(.default, value: colour)
+                .foregroundColor(colours[currentOffset])
+                .animation(.default, value: self.currentOffset)
         }
         .onAppear {
-            delay(seconds: 2) {
-                self.colour = .yellow
+            
+            for index in 1..<colours.count {
+                delay(seconds: Double(index)) {
+                    self.currentOffset = index
+                }
             }
+            
         }
     }
-}
+}//ContentView
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+let colours: [Color] = [
+    Color.cyan,
+    Color.blue,
+    Color.yellow,
+    Color.purple,
+    Color.pink,
+    Color.cyan
+]
