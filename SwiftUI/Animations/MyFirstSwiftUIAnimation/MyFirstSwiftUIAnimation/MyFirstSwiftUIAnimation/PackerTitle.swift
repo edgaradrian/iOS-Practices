8a8e1006cf7f72c3f34903b5654b0f6602b20129
Edgar Adrián <adick.teddy@gmail.com>
2022-05-13 08:12:57 -0500

    Creating Packer Title

---
+++ SwiftUI/Animations/MyFirstSwiftUIAnimation/MyFirstSwiftUIAnimation/MyFirstSwiftUIAnimation/PackerTitle.swift
@@ -0,0 +1,34 @@
+//
+//  PackerTitle.swift
+//  MyFirstSwiftUIAnimation
+//
+//  Created by Edgar Adrián on 13/05/22.
+//
+
+import SwiftUI
+
+struct PackerTitle: View {
+    let title: String
+    let caption: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .shadow(radius: 8)
+                .foregroundColor(.white)
+            
+            Text(caption)
+                .font(.caption)
+                .foregroundColor(.white)
+            
+        }
+    }
+}
+
+struct PackerTitle_Previews: PreviewProvider {
+    static var previews: some View {
+        PackerTitle(title: "Green Bay Packers", caption: "Wisconsin")
+    }
+}
