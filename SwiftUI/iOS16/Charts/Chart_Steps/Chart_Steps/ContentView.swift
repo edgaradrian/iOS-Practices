622dccec6bc2bb238e00b1ff77855993c66f0953
Edgar Adrián <adick.teddy@gmail.com>
2022-06-23 16:19:59 -0500

    Adding an annotation and foreground style

--- SwiftUI/iOS16/Charts/Chart_Steps/Chart_Steps/ContentView.swift
+++ SwiftUI/iOS16/Charts/Chart_Steps/Chart_Steps/ContentView.swift
@@ -9,17 +9,27 @@ import SwiftUI
 import Charts
 
 struct ContentView: View {
+    
+    let semana = Calendar.current.shortWeekdaySymbols
+    let pasos = [1344, 714, 2893, 849, 1101, 2239, 9191]
+    
     var body: some View {
-        Chart {
-            BarMark(
-                x: .value("Día", "Lunes"),
-                y: .value("Pasos", 1344)
-                )
-            
-            BarMark(
-                x: .value("Día", "Martes"),
-                y: .value("Pasos", 714)
-                )
+        VStack(spacing: 10) {
+            Text("Mi primera gráfica")
+                .font(.largeTitle)
+            Chart {
+                ForEach(semana.indices, id: \.self) { index in
+                    BarMark(
+                        x: .value("Día", semana[index]),
+                        y: .value("Pasos", pasos[index])
+                    )
+                    .foregroundStyle(by: .value("Día", semana[index]))
+                    .annotation {
+                        Text("\(pasos[index])")
+                    }
+                }
+            }
+            .padding()
         }
     }
 }
