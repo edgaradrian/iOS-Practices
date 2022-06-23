552dd1e942488f4bdb42a19f7e11b041aae78b33
Edgar Adrián <adick.teddy@gmail.com>
2022-06-23 03:21:51 -0500

    Overlay Introduction

---
+++ SwiftUI/Examples/MyFirstBasicTemplate/MyFirstBasicTemplate/LayoutModifiers/Overlay/Overlay_Intro.swift
@@ -0,0 +1,25 @@
+//
+//  Overlay_Intro.swift
+//  MyFirstBasicTemplate
+//
+//  Created by Edgar Adrián on 23/06/22.
+//
+
+import SwiftUI
+
+struct Overlay_Intro: View {
+    var body: some View {
+        Color.yellow
+            .opacity(0.4)
+            .overlay(
+                HeaderView(titulo: "Superposición", subtitulo: "Introducción", description: "Aquí la vista del cabecero de sobrepone al color para así poner un fondo.")
+            
+            )
+    }
+}
+
+struct Overlay_Intro_Previews: PreviewProvider {
+    static var previews: some View {
+        Overlay_Intro()
+    }
+}
