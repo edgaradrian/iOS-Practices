c4cd3bc90e29e5d99ea9c735f2612ab47f8c955d
Edgar Adrián <adick.teddy@gmail.com>
2022-08-02 15:55:25 -0500

    Localization in SwiftUI

---
+++ SwiftUI/Localization/LocalizationSwiftUI/LocalizationSwiftUI/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  LocalizationSwiftUI
+//
+//  Created by Edgar Adrián on 02/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            Spacer()
+            
+            Text("hello.nurse")
+                .padding()
+            
+            Spacer()
+            
+            Button("continue") {
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/Localization/LocalizationSwiftUI/LocalizationSwiftUI/LocalizationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationSwiftUIApp.swift
+//  LocalizationSwiftUI
+//
+//  Created by Edgar Adrián on 02/08/22.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
