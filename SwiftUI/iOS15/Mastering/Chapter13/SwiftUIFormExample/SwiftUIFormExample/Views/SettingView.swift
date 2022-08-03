975d06d7da80836d92688823b87dcce2c3d25d99
Edgar Adrián <adick.teddy@gmail.com>
2022-08-03 11:26:53 -0500

    Implementing a stepper

--- SwiftUI/iOS15/Mastering/Chapter13/SwiftUIFormExample/SwiftUIFormExample/Views/SettingView.swift
+++ SwiftUI/iOS15/Mastering/Chapter13/SwiftUIFormExample/SwiftUIFormExample/Views/SettingView.swift
@@ -12,6 +12,7 @@ struct SettingView: View {
     private var displayOrders = ["Alfabéticamente", "Favoritos primero", "Registrados primero"]
     @State private var selectedOrder = 0
     @State private var showCheckInOnly = false
+    @State private var maxPriceLevel = 5
     
     var body: some View {
         NavigationView {
@@ -31,6 +32,23 @@ struct SettingView: View {
                     Toggle(isOn: $showCheckInOnly) {
                         Text("Mostrar sólo registrados")
                     }
+                    
+                    Stepper("Mostrar \(String(repeating: "$", count: maxPriceLevel)) o menor") {
+                        self.maxPriceLevel += 1
+                        
+                        if self.maxPriceLevel > 5 {
+                            self.maxPriceLevel = 5
+                        }
+                        
+                    } onDecrement: {
+                        self.maxPriceLevel -= 1
+                        
+                        if self.maxPriceLevel < 1 {
+                            self.maxPriceLevel = 1
+                        }
+                    }
+
+                    
                 } header: {
                     Text("PREFERENCIA DE FILTROS")
                 }
