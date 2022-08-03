1eb1cac1baa92d3b5254b15376e2017424ae30f6
Edgar Adrián <adick.teddy@gmail.com>
2022-08-03 18:45:44 -0500

    Adding preference constants

---
+++ SwiftUI/iOS15/Mastering/Chapter14/SwiftUIFormExample/SwiftUIFormExample/Common/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  SwiftUIFormExample
+//
+//  Created by Edgar Adrián on 03/08/22.
+//
+
+import Foundation
+
+public let PARAMETTER_PREFERENCES_CHECKIN = "view.preferences.showCheckInOnly"
+public let PARAMETTER_PREFERENCES_DISPLAY_ORDER = "view.preferences.displayOrder"
+public let PARAMETTER_PREFERENCES_MAX_PRICE = "view.preferences.maxPriceLevel"
