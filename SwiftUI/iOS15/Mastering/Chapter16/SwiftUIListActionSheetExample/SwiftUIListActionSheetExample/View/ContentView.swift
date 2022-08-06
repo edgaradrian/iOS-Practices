e39d4efc2f899e512b06d5fc832031e2d74d3e58
Edgar Adrián <adick.teddy@gmail.com>
2022-08-06 10:28:07 -0500

    Creating a List of restaurantes

--- SwiftUI/iOS15/Mastering/Chapter16/SwiftUIListActionSheetExample/SwiftUIListActionSheetExample/View/ContentView.swift
+++ SwiftUI/iOS15/Mastering/Chapter16/SwiftUIListActionSheetExample/SwiftUIListActionSheetExample/View/ContentView.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var restaurants = [ Restaurant(name: "Cafe Deadend", image: "cafedeadend"),
+                   Restaurant(name: "Homei", image: "homei"),
+                   Restaurant(name: "Teakha", image: "teakha"),
+                   Restaurant(name: "Cafe Loisl", image: "cafeloisl"),
+                   Restaurant(name: "Petite Oyster", image: "petiteoyster"),
+                   Restaurant(name: "For Kee Restaurant", image: "forkeerestaurant"),
+                   Restaurant(name: "Po's Atelier", image: "posatelier"),
+                   Restaurant(name: "Bourke Street Bakery", image: "bourkestreetbakery"),
+                   Restaurant(name: "Haigh's Chocolate", image: "haighschocolate"),
+                   Restaurant(name: "Palomino Espresso", image: "palominoespresso"),
+                   Restaurant(name: "Homei", image: "upstate"),
+                   Restaurant(name: "Traif", image: "traif"),
+                   Restaurant(name: "Graham Avenue Meats And Deli", image: "grahamavenuemeats"),
+                   Restaurant(name: "Waffle & Wolf", image: "wafflewolf"),
+                   Restaurant(name: "Five Leaves", image: "fiveleaves"),
+                   Restaurant(name: "Cafe Lore", image: "cafelore"),
+                   Restaurant(name: "Confessional", image: "confessional"),
+                   Restaurant(name: "Barrafina", image: "barrafina"),
+                   Restaurant(name: "Donostia", image: "donostia"),
+                   Restaurant(name: "Royal Oak", image: "royaloak"),
+                   Restaurant(name: "CASK Pub and Kitchen", image: "caskpubkitchen")
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(restaurants) { restaurant in
+                    BasicRowImage(restaurant: restaurant)
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle(Text("Restaurantes"))
+        }
     }
-}
+
+}//ContentView
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
