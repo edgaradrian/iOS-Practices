7e110c3a4e5e0dc59d59c4f4c934232daacf1fa2
Edgar Adrián <adick.teddy@gmail.com>
2022-08-09 11:21:30 -0500

    Drag the image around

--- SwiftUI/iOS15/Mastering/Chapter17/SwiftUIGesturesExample/SwiftUIGesturesExample/ContentView2.swift
+++ SwiftUI/iOS15/Mastering/Chapter17/SwiftUIGesturesExample/SwiftUIGesturesExample/ContentView2.swift
@@ -10,16 +10,21 @@ import SwiftUI
 struct ContentView2: View {
     
     @GestureState private var dragOffset = CGSize.zero
+    @State private var position = CGSize.zero
     
     var body: some View {
         Image(systemName: "star.circle.fill")
             .font(.system(size: 80))
-            .offset(x: dragOffset.width, y: dragOffset.height)
+            .offset(x: position.width + dragOffset.width, y: position.height + dragOffset.height)
             .animation(.easeInOut, value: dragOffset)
             .foregroundColor(.yellow)
             .gesture(
                 DragGesture().updating($dragOffset, body: { value, state, transaction in
                     state = value.translation
+                })
+                .onEnded({ value in
+                    self.position.height += value.translation.height
+                    self.position.width += value.translation.width
                 })
             )
     }//body
