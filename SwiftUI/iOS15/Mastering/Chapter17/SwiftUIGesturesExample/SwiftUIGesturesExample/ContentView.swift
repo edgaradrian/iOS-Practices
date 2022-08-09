01b62e3af039cd1a48e84128337fc59a79b1005c
Edgar Adrián <adick.teddy@gmail.com>
2022-08-09 10:43:37 -0500

    Applying a dimming effect when the image is tapped

--- SwiftUI/iOS15/Mastering/Chapter17/SwiftUIGesturesExample/SwiftUIGesturesExample/ContentView.swift
+++ SwiftUI/iOS15/Mastering/Chapter17/SwiftUIGesturesExample/SwiftUIGesturesExample/ContentView.swift
@@ -10,16 +10,21 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var wasTapped = false
+    @GestureState private var longPressTap = false
     
     var body: some View {
         VStack {
             Image(systemName: "star.circle.fill")
                 .font(.system(size: 300))
+                .opacity(longPressTap ? 0.3 : 1)
                 .scaleEffect(wasTapped ? 0.5 : 1)
                 .animation(.easeInOut, value: wasTapped)
                 .foregroundColor(.yellow)
                 .gesture(
                     LongPressGesture(minimumDuration: 1.0)
+                        .updating($longPressTap, body: { currentState, state, transaction in
+                            state = currentState
+                        })
                         .onEnded({ _ in
                             self.wasTapped.toggle()
                         })
