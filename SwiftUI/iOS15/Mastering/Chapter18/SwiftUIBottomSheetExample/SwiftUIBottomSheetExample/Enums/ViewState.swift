71de5cdc08df1366aaee228a6bd18a57ce6cf86a
Edgar Adrián <adick.teddy@gmail.com>
2022-08-11 19:14:21 -0500

    Declaring ViewState enum to represent two view states

---
+++ SwiftUI/iOS15/Mastering/Chapter18/SwiftUIBottomSheetExample/SwiftUIBottomSheetExample/Enums/ViewState.swift
@@ -0,0 +1,15 @@
+//
+//  ViewState.swift
+//  SwiftUIBottomSheetExample
+//
+//  Created by Edgar Adrián on 11/08/22.
+//
+
+import Foundation
+
+enum ViewState {
+    
+    case full
+    case half
+    
+}//ViewState
