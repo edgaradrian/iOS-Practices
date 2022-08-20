c8081f0922d4f153f521a01b83ec5326450b15d1
Edgar Adrián <adick.teddy@gmail.com>
2022-08-20 15:24:52 -0500

    Creating enum type card

---
+++ SwiftUI/iOS15/Mastering/Chapter20/SwiftUIAppleWalletLike/SwiftUIAppleWalletLike/Enums/CardType.swift
@@ -0,0 +1,18 @@
+//
+//  CardType.swift
+//  SwiftUIAppleWalletLike
+//
+//  Created by Edgar Adrián on 20/08/22.
+//
+
+import Foundation
+
+enum CardType: String {
+    
+    case visa
+    case master
+    case american
+    case chase
+    case discover
+    
+}//CardType
---
+++ SwiftUI/iOS15/Mastering/Chapter20/SwiftUIAppleWalletLike/SwiftUIAppleWalletLike/SwiftUIAppleWalletLikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppleWalletLikeApp.swift
+//  SwiftUIAppleWalletLike
+//
+//  Created by Edgar Adrián on 20/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppleWalletLikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
