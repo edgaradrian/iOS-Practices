e322e94901278d12d0c34b32c81fea25900b6f91
Edgar Adrián <adick.teddy@gmail.com>
2022-08-20 16:11:20 -0500

    Implementing Transaction History View

---
+++ SwiftUI/iOS15/Mastering/Chapter20/SwiftUIAppleWalletLike/SwiftUIAppleWalletLike/View/TransactionHistoryView.swift
@@ -0,0 +1,40 @@
+//
+//  TransactionHistoryView.swift
+//  SwiftUIAppleWalletLike
+//
+//  Created by Edgar Adrián on 20/08/22.
+//
+
+import SwiftUI
+
+struct TransactionHistoryView: View {
+    
+    
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            Text("Detalle de Movimientos")
+                .font(.system(size: 25, weight: .black, design: .rounded))
+                .padding()
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 30) {
+                    ForEach(transactions) { transaction in
+                        TransactionView(transaction: transaction)
+                    }
+                }
+                .padding()
+            }
+        }
+        
+    }//body
+    
+    
+    
+}//TransactionHistoryView
+
+struct TransactionHistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        TransactionHistoryView()
+    }
+}
