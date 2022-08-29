0fdfbb4181e96363d62971ef554d1be2801e032a
Edgar Adrián <adick.teddy@gmail.com>
2022-08-29 14:05:08 -0500

    Spreading the cards

--- SwiftUI/iOS15/Mastering/Chapter20/SwiftUIAppleWalletLike/SwiftUIAppleWalletLike/View/WalletView.swift
+++ SwiftUI/iOS15/Mastering/Chapter20/SwiftUIAppleWalletLike/SwiftUIAppleWalletLike/View/WalletView.swift
@@ -21,14 +21,47 @@ struct WalletView: View {
             ZStack {
                 ForEach(cards) { card in
                     CardView(card: card)
+                        .offset(self.offset(for: card))
                         .padding(.horizontal, 35)
+                        .zIndex(self.zIndex(for: card))
                 }
             }
             
             Spacer()
         }
-    }
-}
+    }//body
+    
+    private func index(for card: Card) -> Int? {
+        
+        guard let index = cards.firstIndex(where: { $0.id == card.id }) else {
+            return nil
+        }
+        
+        return index
+        
+    }//index
+    
+    private func zIndex(for card: Card) -> Double {
+        
+        guard let cardIndex = index(for: card) else {
+            return 0.0
+        }
+        
+        return -Double(cardIndex)
+        
+    }//zIndex
+    
+    private func offset(for card: Card) -> CGSize {
+        
+        guard let cardIndex = index(for: card) else {
+            return CGSize()
+        }
+        
+        return CGSize(width: 0, height: -50 * CGFloat(cardIndex))
+        
+    }//offset
+    
+}//WalletView
 
 struct WalletView_Previews: PreviewProvider {
     static var previews: some View {
