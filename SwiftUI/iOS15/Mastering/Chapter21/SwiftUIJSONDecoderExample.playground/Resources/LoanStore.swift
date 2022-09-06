1f1ffc6dc10717aa5201e094b50516bbfd80548d
Edgar Adrián <adick.teddy@gmail.com>
2022-09-05 20:58:39 -0500

    Calling the Web API

--- SwiftUI/iOS15/Mastering/Chapter21/SwiftUIJSONDecoderExample.playground/Resources/LoanStore.swift
+++ SwiftUI/iOS15/Mastering/Chapter21/SwiftUIJSONDecoderExample.playground/Resources/LoanStore.swift
@@ -3,6 +3,7 @@ import SwiftUI
 
 class LoanStore: Decodable, ObservableObject {
     @Published var loans: [Prestamo] = []
+    private static var kivaLoanURL = "https://api.kivaws.org/v1/loans/newest.json"
     
     enum CodingKeys: CodingKey {
         
@@ -18,4 +19,45 @@ class LoanStore: Decodable, ObservableObject {
     init() {
         
     }
+    
+    func fetchLatestLoans() {
+        
+        guard let loanURL = URL(string: self.kivaLoanURL) else {
+            return
+        }
+        
+        let request = URLRequest(url: loanURL)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            if let data = data {
+                DispatchQueue.main.async {
+                    self.loans = self.parseJsonData(data: data)
+                }
+            }
+            
+        }
+        
+    }//fetchLatestLoans
+    
+    func parseJsonData(data: Data) -> [Prestamo] {
+        let decoder = JSONDecoder()
+        
+        do {
+            
+            let loanStore = try decoder.decode(LoanStore.self, from: data)
+            self.loans = loanStore.loans
+            
+        } catch {
+            print(error)
+        }
+        
+        return loans
+        
+    }//parseJsonData
+    
 }//LoanStore
