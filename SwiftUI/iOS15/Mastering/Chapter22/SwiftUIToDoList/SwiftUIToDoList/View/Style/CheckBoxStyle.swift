fce5468758446a6aaf7a439dc02b8bfefd6bc977
Edgar Adrián <adick.teddy@gmail.com>
2022-09-08 21:04:27 -0500

    Adding CheckBoxStyle

---
+++ SwiftUI/iOS15/Mastering/Chapter22/SwiftUIToDoList/SwiftUIToDoList/View/Style/CheckBoxStyle.swift
@@ -0,0 +1,32 @@
+//
+//  CheckBoxStyle.swift
+//  SwiftUIToDoList
+//
+//  Created by Edgar Adrián on 08/09/22.
+//
+
+import SwiftUI
+
+struct CheckBoxStyle: ToggleStyle {
+    
+    func makeBody(configuration: Configuration) -> some View {
+        return HStack {
+            
+            Image(systemName: configuration.isOn ? "checkmark.circle.fill" : "circle")
+                .resizable()
+                .frame(width: 24, height: 24)
+                .foregroundColor(configuration.isOn ? .purple : .gray)
+                .font(.system(size: 20, weight: .bold, design: .default))
+                .onTapGesture {
+                    configuration.isOn.toggle()
+                }
+            
+            
+            configuration.label
+                
+        }
+    }//makeBody
+    
+}//CheckBoxStyle
+
+
