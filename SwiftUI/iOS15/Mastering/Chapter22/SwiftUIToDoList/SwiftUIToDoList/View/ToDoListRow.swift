82751d9ec5c643fe5245df0a59545ceb76ba40d8
Edgar Adrián <adick.teddy@gmail.com>
2022-09-09 11:32:39 -0500

    Adding ToDoListRow

---
+++ SwiftUI/iOS15/Mastering/Chapter22/SwiftUIToDoList/SwiftUIToDoList/View/ToDoListRow.swift
@@ -0,0 +1,47 @@
+//
+//  ToDoListRow.swift
+//  SwiftUIToDoList
+//
+//  Created by Edgar Adrián on 09/09/22.
+//
+
+import SwiftUI
+
+struct ToDoListRow: View {
+    
+    @ObservedObject var todoItem: ToDoItem
+    
+    var body: some View {
+        Toggle(isOn: self.$todoItem.isComplete) {
+            HStack {
+                Text(self.todoItem.name)
+                    .strikethrough(self.todoItem.isComplete, color: .black)
+                    .bold()
+                    .animation(.default)
+                
+                Spacer()
+                
+                Circle()
+                    .frame(width: 10, height: 10)
+                    .foregroundColor(self.color(for: self.todoItem.priority))
+            }
+        }.toggleStyle(CheckBoxStyle())
+    }//body
+    
+    private func color(for priority: Priority) -> Color {
+        switch priority {
+        case .high: return .red
+        case .normal: return .orange
+        case .low: return .green
+        }
+    }//color
+    
+}//ToDoListRow
+
+struct ToDoListRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoListRow(todoItem: ToDoItem(name: "Learning Code", priority: .high, isComplete: true))
+            .padding()
+            .previewLayout(.sizeThatFits)
+    }
+}//ToDoListRow_Previews
