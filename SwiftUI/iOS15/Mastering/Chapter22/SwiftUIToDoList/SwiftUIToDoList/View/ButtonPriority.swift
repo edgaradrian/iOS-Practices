1ee0104ef78b271f635f3dbf1c4ef3097f713d10
Edgar Adrián <adick.teddy@gmail.com>
2022-10-13 12:05:19 -0500

    Creating Data Model

---
+++ SwiftUI/iOS15/Mastering/Chapter22/SwiftUIToDoList/SwiftUIToDoList/View/ButtonPriority.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonPriority.swift
+//  SwiftUIToDoList
+//
+//  Created by Edgar Adrián on 08/09/22.
+//
+
+import SwiftUI
+
+struct ButtonPriority: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ButtonPriority_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonPriority()
+    }
+}
---
+++ SwiftUI/iOS15/Mastering/Chapter25/SwiftUIPersonalFinance/SwiftUIPersonalFinance/SwiftUIPersonalFinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPersonalFinanceApp.swift
+//  SwiftUIPersonalFinance
+//
+//  Created by Edgar Adrián on 13/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPersonalFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
