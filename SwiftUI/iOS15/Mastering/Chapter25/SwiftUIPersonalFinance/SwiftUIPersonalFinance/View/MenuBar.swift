5dc3325fa87dbf6d58d9c32b924b9a80284d656c
Edgar Adrián <adick.teddy@gmail.com>
2022-11-05 20:20:09 -0600

    Adding MenuBar

---
+++ SwiftUI/iOS15/Mastering/Chapter25/SwiftUIPersonalFinance/SwiftUIPersonalFinance/View/MenuBar.swift
@@ -0,0 +1,49 @@
+//
+//  MenuBar.swift
+//  SwiftUIPersonalFinance
+//
+//  Created by Edgar Adrián on 05/11/22.
+//
+
+import SwiftUI
+
+struct MenuBar<Content>: View where Content: View {
+    @State private var showPaymentForm = false
+    
+    let modalContent: () -> Content
+    
+    var body: some View {
+        ZStack(alignment: .trailing) {
+            HStack(alignment: .center) {
+                Spacer()
+                
+                VStack(alignment: .center) {
+                    Text(Date.today.string(with: "EEEE, MMM d, yyyy"))
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                    Text("Finanzas Personal")
+                        .font(.title)
+                        .fontWeight(.black)
+                }
+                
+                Spacer()
+            }
+            
+            Button(action: {
+                self.showPaymentForm = true
+            }) {
+                Image(systemName: "plus.circle")
+                    .font(.title)
+                    .foregroundColor(.primary)
+            }
+
+            .sheet(isPresented: self.$showPaymentForm, onDismiss: {
+                self.showPaymentForm = false
+            }) {
+                self.modalContent()
+            }
+        }
+
+    }
+}//MenuBar
+
