1c6925b2e81816fc5097347a57d013261ec2b76d
Edgar Adrián <ingeniero@skiff.com>
2023-10-02 23:52:49 -0600

    Changing the text

---
+++ Mastering15/Chap2/SwiftUIText/SwiftUIText/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIText
+//
+//  Created by Edgar Adrián on 30/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mastering15/Chap2/SwiftUIText/SwiftUIText/SwiftUITextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITextApp.swift
+//  SwiftUIText
+//
+//  Created by Edgar Adrián on 30/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
