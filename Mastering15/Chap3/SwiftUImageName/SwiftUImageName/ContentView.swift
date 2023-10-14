19c014597ade9c202586f6d01bef52462b38a7dc
Edgar Adrián <ingeniero@skiff.com>
2023-10-14 17:31:18 -0600

    Display a system image

---
+++ Mastering15/Chap3/SwiftUImageName/SwiftUImageName/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUImageName
+//
+//  Created by Edgar Adrián on 14/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mastering15/Chap3/SwiftUImageName/SwiftUImageName/SwiftUImageNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUImageNameApp.swift
+//  SwiftUImageName
+//
+//  Created by Edgar Adrián on 14/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUImageNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
