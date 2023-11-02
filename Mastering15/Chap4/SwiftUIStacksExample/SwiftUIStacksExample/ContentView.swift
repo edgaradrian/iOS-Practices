ffa7d825d6b95b2828553f7825eea56e89138778
Edgar Adrián <ingeniero@skiff.com>
2023-11-02 14:15:23 -0600

    Extract subview

--- Mastering15/Chap4/SwiftUIStacksExample/SwiftUIStacksExample/ContentView.swift
+++ Mastering15/Chap4/SwiftUIStacksExample/SwiftUIStacksExample/ContentView.swift
@@ -10,19 +10,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            VStack(alignment: .leading, spacing: 2) {
-                Text("Elige")
-                    .font(.system(.largeTitle, design: .rounded))
-                    .fontWeight(.black)
-                Text("Tu plan")
-                    .font(.system(.largeTitle, design: .rounded))
-                    .fontWeight(.black)
-            }
-            .padding()
+            HeaderView()
         }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct HeaderView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 2) {
+            Text("Elige")
+                .font(.system(.largeTitle, design: .rounded))
+                .fontWeight(.black)
+            Text("Tu plan")
+                .font(.system(.largeTitle, design: .rounded))
+                .fontWeight(.black)
+        }
+        .padding()
+    }
 }
