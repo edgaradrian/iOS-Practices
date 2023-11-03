ccd9c84f1d49efe140eda7fd980fd093c51c003f
Edgar Adrián <ingeniero@skiff.com>
2023-11-03 13:35:18 -0600

    Exercise #1

--- Mastering15/Chap4/SwiftUIStacksExample/SwiftUIStacksExample/ContentView.swift
+++ Mastering15/Chap4/SwiftUIStacksExample/SwiftUIStacksExample/ContentView.swift
@@ -14,21 +14,15 @@ struct ContentView: View {
             
             HStack {
                 PricingView(title: "Básico", price: "$89", textColor: .white, bgColor: .cyan)
+
+                PricingView(title: "Pro", price: "$159", textColor: .black, bgColor: .red, promotionalText: "Opción para estudiantes")
                 
-                ZStack {
-                    PricingView(title: "Pro", price: "$159", textColor: .black, bgColor: .red)
-                    
-                    Text("Students option")
-                        .font(.system(.caption, design: .rounded))
-                        .fontWeight(.bold)
-                        .foregroundStyle(.white)
-                        .padding(5)
-                        .background(Color(red: 255/255, green: 183/255, blue: 37/255))
-                        .offset(x: 0, y: 80)
-                    
-                }
+                
             }
             
+            PricingView(title: "Equipo", price: "$1499", textColor: .green, bgColor: .yellow, imageName: "wand.and.rays", promotionalText: "Para equipos de 15")
+            
+            Spacer()
         }
     }
 }
@@ -39,15 +33,19 @@ struct ContentView: View {
 
 struct HeaderView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 2) {
-            Text("Elige")
-                .font(.system(.largeTitle, design: .rounded))
-                .fontWeight(.black)
-            Text("Tu plan")
-                .font(.system(.largeTitle, design: .rounded))
-                .fontWeight(.black)
+        HStack {
+            VStack(alignment: .leading, spacing: 2) {
+                Text("Elige")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .fontWeight(.black)
+                Text("Tu plan")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .fontWeight(.black)
+            }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
@@ -57,26 +55,48 @@ struct PricingView: View {
     var price: String
     var textColor: Color
     var bgColor: Color
+    var imageName = ""
+    var promotionalText = ""
     
     var body: some View {
-        VStack {
+        
+        ZStack {
+            VStack {
+                
+                Image(systemName: imageName)
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.black)
+                    .foregroundStyle(textColor)
+                
+                Text(title)
+                    .font(.system(.title, design: .rounded))
+                    .fontWeight(.black)
+                    .foregroundStyle(textColor)
+                
+                Text(price)
+                    .font(.system(size: 40, weight: .heavy, design: .rounded))
+                    .foregroundStyle(textColor)
+                
+                Text("Por mes")
+                    .font(.headline)
+                    .foregroundStyle(textColor)
+                
+            }
+            .padding(40)
+            .background(bgColor)
+            .clipShape(Circle())
+            .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, minHeight: 100)
             
-            Text(title)
-                .font(.system(.title, design: .rounded))
-                .fontWeight(.black)
-                .foregroundStyle(textColor)
-            
-            Text(price)
-                .font(.system(size: 40, weight: .heavy, design: .rounded))
-                .foregroundStyle(textColor)
-            
-            Text("Por mes")
-                .font(.headline)
-                .foregroundStyle(textColor)
-            
+            if promotionalText.count > 0 {
+                Text(promotionalText)
+                    .font(.system(.caption, design: .rounded))
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                    .padding(5)
+                    .background(Color(red: 255/255, green: 183/255, blue: 37/255))
+                    .offset(x: 0, y: 90)
+            }
         }
-        .padding(40)
-        .background(bgColor)
-        .clipShape(Circle())
+   
     }
 }
