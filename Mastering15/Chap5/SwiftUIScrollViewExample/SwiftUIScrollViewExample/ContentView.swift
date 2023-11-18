98f0f3d3486bc399098e937792624a9e587477bc
Edgar Adrián <ingeniero@skiff.com>
2023-11-17 23:08:01 -0600

    Using ScrollView

--- Mastering15/Chap5/SwiftUIScrollViewExample/SwiftUIScrollViewExample/ContentView.swift
+++ Mastering15/Chap5/SwiftUIScrollViewExample/SwiftUIScrollViewExample/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            VStack {
+                CardView(image: "swiftui-button", category: "SwiftUI", heading: "Dibujando un borde con esquinas redondeadas", author: "Por Edgar Adrián")
+                
+                CardView(image: "macos-programming", category: "macOS", heading: "Construyendo una app de edición simple", author: "Por Edgar Adrián")
+                
+                CardView(image: "flutter-app", category: "Flutter", heading: "Construyendo un complejo diseño con FLutter", author: "Por Edgar Adrián")
+                
+                CardView(image: "natural-language-api", category: "iOS", heading: "¿Qué hay de nuevo con la API?", author: "Por Edgar Adrián")
+            }
         }
-        .padding()
     }
 }
 
