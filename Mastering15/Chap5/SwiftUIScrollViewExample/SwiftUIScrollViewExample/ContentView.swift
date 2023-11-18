1d9e2fe0731f37e071bed711de11bca62d6f4bbe
Edgar Adrián <ingeniero@skiff.com>
2023-11-17 23:37:04 -0600

    Creating a Carousel UI with Horizontal ScrollView

--- Mastering15/Chap5/SwiftUIScrollViewExample/SwiftUIScrollViewExample/ContentView.swift
+++ Mastering15/Chap5/SwiftUIScrollViewExample/SwiftUIScrollViewExample/ContentView.swift
@@ -9,16 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ScrollView {
-            VStack {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
                 CardView(image: "swiftui-button", category: "SwiftUI", heading: "Dibujando un borde con esquinas redondeadas", author: "Por Edgar Adrián")
+                    .frame(width: 300)
                 
                 CardView(image: "macos-programming", category: "macOS", heading: "Construyendo una app de edición simple", author: "Por Edgar Adrián")
+                    .frame(width:300)
                 
                 CardView(image: "flutter-app", category: "Flutter", heading: "Construyendo un complejo diseño con FLutter", author: "Por Edgar Adrián")
+                    .frame(width:300)
                 
                 CardView(image: "natural-language-api", category: "iOS", heading: "¿Qué hay de nuevo con la API?", author: "Por Edgar Adrián")
+                    .frame(width:300)
             }
+            
+            Spacer()
         }
     }
 }
