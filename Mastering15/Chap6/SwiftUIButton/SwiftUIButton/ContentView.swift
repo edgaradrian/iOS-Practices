95c863318930dba2528d196ec822f5d33b238bdb
Edgar Adrián <ingeniero@skiff.com>
2024-03-11 11:57:03 -0600

    feat(chap6): Creating a simple Button

---
+++ Mastering15/Chap6/SwiftUIButton/SwiftUIButton/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUIButton
+//
+//  Created by Edgar Adrián on 11/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button {
+                print("Fui pulsado")
+            } label: {
+                Text("Púlsame")
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mastering15/Chap6/SwiftUIButton/SwiftUIButton/SwiftUIButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIButtonApp.swift
+//  SwiftUIButton
+//
+//  Created by Edgar Adrián on 11/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
